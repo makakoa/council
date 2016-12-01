82d8f031dad63c101d578dd23a40dfb2f68c6669
Nolan Yee <skinnyfatman@gmail.com>
2016-12-01 00:11:00 -0800

    fixed routing

--- app/server.js
+++ app/server.js
@@ -6,9 +6,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req,res){
+app.get('/*', function(req,res){
   req;
-  res.sendFile(__dirname + 'public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 var port = process.env.PORT || 3010;
